ad90a9c2c4d36bbf7dd3083ac0d92053157f1c78
Shivang Sharma <sharmashivang1999@gmail.com>
2024-09-14 19:39:15 +0530

    feat: library layout design added

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,7 +1,28 @@
 "use client";
 
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
+
 const Library = () => {
-  return <div>Library!</div>;
+  const handleUpload = () => {
+    // handle upload later
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+        </div>
+        <AiOutlinePlus
+          onClick={handleUpload}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+    </div>
+  );
 };
 
 export default Library;
