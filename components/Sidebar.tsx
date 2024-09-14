3ee067fd52cbed29aca6b038cb1ddc46127e4f01
Shivang Sharma <sharmashivang1999@gmail.com>
2024-09-14 23:47:04 +0530

    feat: header layout design

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -48,7 +48,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           <Library />
         </Box>
       </div>
-      <main>{children}</main>
+      <main className="h-full flex-1 overflow-y-auto py-2 px-2">
+        {children}
+      </main>
     </div>
   );
 };
