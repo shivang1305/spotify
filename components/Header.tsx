a23982060ac893ff63b663d12ba924595fadc34d
Shivang Sharma <sharmashivang1999@gmail.com>
2024-10-08 16:20:54 +0530

    feat: Toast provider added for auth toasts

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
@@ -31,7 +32,8 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 
     router.refresh();
 
-    if (error) console.log(error);
+    if (error) toast.error(error.message);
+    else toast.success("Logged Out");
   };
 
   return (
