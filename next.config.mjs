f827bf7425d3fa0f9fe0ceece503ac7750f6f5c1
Shivang Sharma <sharmashivang1999@gmail.com>
2024-11-20 14:10:22 +0530

    feat: song fetching list integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["faoianyqassdsgkpqffn.supabase.co"],
+  },
+};
 
 export default nextConfig;
