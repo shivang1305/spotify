3db2311c9d5f5600901535cdbe0d13ce712f9012
Shivang Sharma <sharmashivang1999@gmail.com>
2024-11-24 11:56:12 +0530

    feat: song search functionality added

---
+++ actions/getSongsByTitle.ts
@@ -0,0 +1,21 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getSongsByTitle = async(title: string): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    });
+
+    const {data, error} = await supabase
+        .from("songs")
+        .select("*")
+        .ilike("title", `%${title}%`)
+        .order("created_at", {ascending: false});
+
+    if(error) console.log(error);
+
+    return data || [];
+}
+
+export default getSongsByTitle;
