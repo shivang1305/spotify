7d6f8ad98fb3ad91b27961ba7b8af9ff20d749e5
Shivang Sharma <sharmashivang1999@gmail.com>
2024-11-25 12:44:51 +0530

    feat: get liked songs action created

---
+++ actions/getLikedSongs.ts
@@ -0,0 +1,33 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getLikedSongs = async() : Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    });
+
+    const {
+        data: {session}
+    } = await supabase.auth.getSession();
+
+    const {data, error} = await supabase
+        .from("liked_songs")
+        .select("*, songs(*)") // to fetch song details along
+        .eq("user_id", session?.user?.id)
+        .order("created_at", {ascending: false});
+
+    if(error) {
+        console.log(error.message);
+        return [];
+    }
+
+    if(!data) return [];
+
+    // to return the full details of the song fetched from songs table
+    return data.map((item) => (
+        {...item.songs}
+    ));
+}
+
+export default getLikedSongs;
