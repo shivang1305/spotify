fc031d9445dbccfa3ec77a04ad56d9d87e52c621
Shivang Sharma <sharmashivang1999@gmail.com>
2024-12-05 15:17:36 +0530

    chore: prettier formatting

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,12 +1,9 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ?? "",
-  {
-    apiVersion: "2024-06-20",
-    appInfo: {
-      name: "Spotify Music Player",
-      version: "0.1.0"
-    }
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: "2024-06-20",
+  appInfo: {
+    name: "Spotify Music Player",
+    version: "0.1.0",
+  },
+});
