3c3fcdd4e42d58217653ef0b843c52f4eed4525f
Shivang Sharma <sharmashivang1999@gmail.com>
2024-12-28 14:35:42 +0530

    feat: subscribe modal added

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -3,9 +3,14 @@
 import AuthModal from "@/components/AuthModal";
 import SubscribeModal from "@/components/SubscribeModal";
 import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
 import { useEffect, useState } from "react";
 
-const ModalProvider = () => {
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -18,7 +23,7 @@ const ModalProvider = () => {
     <>
       <AuthModal />
       <UploadModal />
-      <SubscribeModal />
+      <SubscribeModal products={products} />
     </>
   );
 };
